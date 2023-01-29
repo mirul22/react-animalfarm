d86c28dc760ed9db30afed18a03ad974a4d58f9f
mirul22~ <amirulikmal.biz@gmail.com>
2023-01-29 13:54:40 +0800

    update - node query data

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+// Init the express app
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//make some animals
+import Chance from "chance";
+const chance = new Chance();
+
+const animals = [...Array(250).keys()].map((id) => {
+  return {
+    id,
+    type: chance.animal(),
+    age: chance.age(),
+    name: chance.name(),
+  };
+});
+
+// endpoint to search for animals
+app.get("", (req, res) => {
+  //filter result by query
+  const q = req.query.q?.toLowerCase() || "";
+  const results = animals.filter((animal) =>
+    animal.type.toLowerCase().includes(q)
+  );
+
+  res.send(results);
+});
+
+app.listen(8080, () => console.log("Listening on port http://localhost:8080"));
